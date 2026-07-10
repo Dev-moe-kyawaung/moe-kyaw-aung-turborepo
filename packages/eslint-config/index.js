14b8809f9bdae01b72d6f1cc910b17ffbabddf87
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-07-11 05:48:41 +0630

    Create index.js

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: path.resolve(__dirname),
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { ignoreRestSiblings: true },
+    ],
+    "react/react-in-jsx-scope": "off",
+  },
+};

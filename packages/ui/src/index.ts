72be2e6383a70c4f4ac6b4d9a2f7af38cd2bab21
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-07-11 05:53:46 +0630

    Create index.ts

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,6 @@
+export { Button } from "./components/Button";
+export { AppCard } from "./components/AppCard";
+export { Section } from "./components/Section";
+export { Avatar } from "./components/Avatar";
+export { SocialLink } from "./components/SocialLink";
+export { TechChip } from "./components/TechChip";

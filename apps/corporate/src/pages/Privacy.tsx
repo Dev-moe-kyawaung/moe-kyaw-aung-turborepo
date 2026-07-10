dc30d8304f67d574aa230c48426fea52cbf07327
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-07-11 06:24:12 +0630

    Create Privacy.tsx

---
+++ apps/corporate/src/pages/Privacy.tsx
@@ -0,0 +1,11 @@
+import { Section } from "ui";
+
+export default function Privacy() {
+  return (
+    <Section id="privacy" label="Legal" title="Privacy Policy">
+      <p className="text-secondary-100">
+        This is a placeholder for your privacy policy content.
+      </p>
+    </Section>
+  );
+}
